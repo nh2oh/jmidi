57368801d22ec9f7d6a9de5931ec7fd2f30c32fb
ben knowles <ben_junk@ionophore.com>
2019-07-21 08:16:13 -0500

    Assorted cleanups to small_bytevec_t

--- mtrk_event_t_internal.cpp
+++ mtrk_event_t_internal.cpp
@@ -1,72 +1,72 @@
 #include "mtrk_event_t_internal.h"
-#include "midi_raw.h"
 #include <cstdint>
+#include <cstdlib>  // std::abort()
 #include <algorithm>  // std::clamp(), std::max(), std::copy()
 
 namespace mtrk_event_t_internal {
 
-void small_t::init() {
+void small_t::init() noexcept {
 	this->flags_ = 0x80u;
 	this->d_.fill(0x00u);
 }
-int32_t small_t::size() const {
+int32_t small_t::size() const noexcept {
 	this->abort_if_not_active();
 	return ((this->flags_)&0x7Fu);
 }
-constexpr int32_t small_t::capacity() const {
+constexpr int32_t small_t::capacity() const noexcept {
 	return small_t::size_max;
 }
-int32_t small_t::resize(int32_t sz) {
+int32_t small_t::resize(int32_t sz) noexcept {
 	this->abort_if_not_active();
 	sz = std::clamp(sz,0,this->capacity());
 	this->flags_ = (static_cast<unsigned char>(sz)|0x80u);
 	return ((this->flags_)&0x7Fu);
 }
-void small_t::abort_if_not_active() const {
+void small_t::abort_if_not_active() const noexcept {
 	if (!((this->flags_)&0x80u)) {
 		std::abort();
 	}
 }
-unsigned char *small_t::begin() {
+unsigned char *small_t::begin() noexcept {
 	this->abort_if_not_active();
 	return &(this->d_[0]);
 }
-const unsigned char *small_t::begin() const {
+const unsigned char *small_t::begin() const noexcept {
 	this->abort_if_not_active();
 	return &(this->d_[0]);
 }
-unsigned char *small_t::end() {
+unsigned char *small_t::end() noexcept {
 	this->abort_if_not_active();
 	return &(this->d_[0]) + this->size();
 }
-const unsigned char *small_t::end() const {
+const unsigned char *small_t::end() const noexcept {
 	this->abort_if_not_active();
 	return &(this->d_[0]) + this->size();
 }
 //-----------------------------------------------------------------------------
-void big_t::init() {
+void big_t::init() noexcept {
 	this->flags_=0x00u;
 	this->pad_.fill(0x00u);
 	this->p_ = nullptr;
 	this->sz_ = 0u;
 	this->cap_ = 0u;
 }
-void big_t::free_and_reinit() {
+void big_t::free_and_reinit() noexcept {
 	this->abort_if_not_active();
 	if (this->p_) {
 		delete [] this->p_;
 	}
 	this->init();
 }
-int32_t big_t::size() const {
+int32_t big_t::size() const noexcept {
 	this->abort_if_not_active();
 	return static_cast<int32_t>(this->sz_);
 }
-int32_t big_t::capacity() const {
+int32_t big_t::capacity() const noexcept {
 	this->abort_if_not_active();
 	return static_cast<int32_t>(this->cap_);
 }
-void big_t::abort_if_not_active() const {
+void big_t::abort_if_not_active() const noexcept {
 	if ((this->flags_)&0x80u) {
 		std::abort();
 	}
@@ -77,7 +77,7 @@ void big_t::abort_if_not_active() const {
 	}
 }
 void big_t::adopt(const big_t::pad_t& pad, unsigned char *ptr, 
-					int32_t sz, int32_t cap) {
+					int32_t sz, int32_t cap) noexcept {
 	this->abort_if_not_active();
 
 	this->pad_ = pad;
@@ -90,9 +90,6 @@ void big_t::adopt(const big_t::pad_t& pad, unsigned char *ptr,
 }
 int32_t big_t::resize(int32_t new_sz) {
 	this->abort_if_not_active();
-	// Keeps the object 'big', even if the new size would allow it to
-	// fit in a small_t.  big_t/small_t switching logic lives in sbo_t,
-	// not here.  
 	new_sz = std::clamp(new_sz,0,big_t::size_max);
 	if (new_sz <= this->capacity()) {
 		this->sz_ = static_cast<uint32_t>(new_sz);
@@ -117,33 +114,33 @@ int32_t big_t::reserve(int32_t new_cap) {
 	}
 	return this->capacity();
 }
-unsigned char *big_t::begin() {
+unsigned char *big_t::begin() noexcept {
 	this->abort_if_not_active();
 	return this->p_;
 }
-const unsigned char *big_t::begin() const {
+const unsigned char *big_t::begin() const noexcept {
 	this->abort_if_not_active();
 	return this->p_;
 }
-unsigned char *big_t::end() {
+unsigned char *big_t::end() noexcept {
 	this->abort_if_not_active();
 	return this->p_ + this->sz_;
 }
-const unsigned char *big_t::end() const {
+const unsigned char *big_t::end() const noexcept {
 	this->abort_if_not_active();
 	return this->p_ + this->sz_;
 }
 //-----------------------------------------------------------------------------
-sbo_t::sbo_t() {
+small_bytevec_t::small_bytevec_t() noexcept {
 	this->init_small();
 }
-void sbo_t::init_small() {
+void small_bytevec_t::init_small() noexcept {
 	this->u_.s_.init();
 }
-void sbo_t::init_big() {
+void small_bytevec_t::init_big() noexcept {
 	this->u_.b_.init();
 }
-sbo_t::sbo_t(const sbo_t& rhs) {  // Copy ctor
+small_bytevec_t::small_bytevec_t(const small_bytevec_t& rhs) {  // Copy ctor
 	// This is a ctor; *this is in an uninitialized state
 	if (rhs.is_big()) {
 		this->init_big();
@@ -153,14 +150,14 @@ sbo_t::sbo_t(const sbo_t& rhs) {  // Copy ctor
 	this->resize(rhs.size());
 	std::copy(rhs.begin(),rhs.end(),this->begin());
 }
-sbo_t& sbo_t::operator=(const sbo_t& rhs) {  // Copy assign
+small_bytevec_t& small_bytevec_t::operator=(const small_bytevec_t& rhs) {  // Copy assign
 	// TODO:  Can be optimized; resize() copies the old data when new[]'ing
 	// a new buffer.  
 	this->resize(rhs.size());
 	std::copy(rhs.begin(),rhs.end(),this->begin());
 	return *this;
 }
-sbo_t::sbo_t(sbo_t&& rhs) noexcept {  // Move ctor
+small_bytevec_t::small_bytevec_t(small_bytevec_t&& rhs) noexcept {  // Move ctor
 	// This is a ctor; *this is in an uninitialized state
 	if (rhs.is_big()) {
 		this->init_big();
@@ -175,7 +172,7 @@ sbo_t::sbo_t(sbo_t&& rhs) noexcept {  // Move ctor
 		rhs.init_small();
 	}
 }
-sbo_t& sbo_t::operator=(sbo_t&& rhs) noexcept {  // Move assign
+small_bytevec_t& small_bytevec_t::operator=(small_bytevec_t&& rhs) noexcept {  // Move assign
 	// TODO:  Can be optimized; resize() copies the old data when new[]'ing
 	// a new buffer.  
 	if (this->is_big()) {
@@ -196,36 +193,36 @@ sbo_t& sbo_t::operator=(sbo_t&& rhs) noexcept {  // Move assign
 	return *this;
 }
 
-sbo_t::~sbo_t() {  // Dtor
+small_bytevec_t::~small_bytevec_t() noexcept {  // Dtor
 	if (this->is_small()) {
 		this->init_small();
 	} else {
 		this->u_.b_.free_and_reinit();
 	}
 }
-int32_t sbo_t::size() const {
+int32_t small_bytevec_t::size() const noexcept {
 	if (this->is_small()) {
 		return this->u_.s_.size();
 	} else {
 		return this->u_.b_.size();
 	}
 }
-int32_t sbo_t::capacity() const {
+int32_t small_bytevec_t::capacity() const noexcept {
 	if (this->is_small()) {
 		return this->u_.s_.capacity();
 	} else {
 		return this->u_.b_.capacity();
 	}
 }
-int32_t sbo_t::resize(int32_t new_sz) {
-	new_sz = std::clamp(new_sz,0,sbo_t::size_max);
+int32_t small_bytevec_t::resize(int32_t new_sz) {
+	new_sz = std::clamp(new_sz,0,small_bytevec_t::size_max);
 	if (this->is_big()) {
 		if (new_sz <= small_t::size_max) {  // Resize big->small
 			// Note that even though the present object is big and the new
 			// size is <= small_t::size_max, it is still possible that the
 			// size of the present object is < new_sz.  
 			// TODO:  
-			// sbo_t yay=sbo_t();
+			// small_bytevec_t yay=small_bytevec_t();
 			// std::copy(this->begin(),this->begin()+n_bytes2copy,yay.begin());
 			// *this=yay;
 			// (ie, reuse the copy ctor); won't have this messy delete []
@@ -256,8 +253,8 @@ int32_t sbo_t::resize(int32_t new_sz) {
 	}
 	return new_sz;
 }
-int32_t sbo_t::reserve(int32_t new_cap) {
-	new_cap = std::clamp(new_cap,this->capacity(),sbo_t::size_max);
+int32_t small_bytevec_t::reserve(int32_t new_cap) {
+	new_cap = std::clamp(new_cap,this->capacity(),small_bytevec_t::size_max);
 	if (this->is_big()) {
 		new_cap = this->u_.b_.reserve(new_cap);
 	} else {  // present object is small
@@ -272,7 +269,7 @@ int32_t sbo_t::reserve(int32_t new_cap) {
 	}
 	return this->capacity();
 }
-unsigned char *sbo_t::push_back(unsigned char c) {
+unsigned char *small_bytevec_t::push_back(unsigned char c) {
 	auto sz = this->size();
 	if (sz==this->capacity()) {
 		// Note that a consequence of this test is that if this->is_small(),
@@ -282,7 +279,7 @@ unsigned char *sbo_t::push_back(unsigned char c) {
 
 		// if this->size()==0, don't want to reserve 2*sz==0; in theory
 		// one could have a capacity 0 'big' object.  
-		auto new_sz = std::max(2*sz,sbo_t::capacity_small);
+		auto new_sz = std::max(2*sz,small_bytevec_t::capacity_small);
 		this->reserve(new_sz);
 	}
 	this->resize(sz+1);
@@ -290,53 +287,53 @@ unsigned char *sbo_t::push_back(unsigned char c) {
 	return this->end()-1;
 }
 
-bool sbo_t::debug_is_big() const {
+bool small_bytevec_t::debug_is_big() const noexcept {
 	return this->is_big();
 }
-unsigned char *sbo_t::begin() {
+unsigned char *small_bytevec_t::begin() noexcept {
 	if (this->is_small()) {
 		return this->u_.s_.begin();
 	} else {
 		return this->u_.b_.begin();
 	}
 }
-const unsigned char *sbo_t::begin() const {
+const unsigned char *small_bytevec_t::begin() const noexcept {
 	if (this->is_small()) {
 		return this->u_.s_.begin();
 	} else {
 		return this->u_.b_.begin();
 	}
 }
-unsigned char *sbo_t::end() {
+unsigned char *small_bytevec_t::end() noexcept {
 	if (this->is_small()) {
 		return this->u_.s_.end();
 	} else {
 		return this->u_.b_.end();
 	}
 }
-const unsigned char *sbo_t::end() const {
+const unsigned char *small_bytevec_t::end() const noexcept {
 	if (this->is_small()) {
 		return this->u_.s_.end();
 	} else {
 		return this->u_.b_.end();
 	}
 }
-unsigned char *sbo_t::raw_begin() {
+unsigned char *small_bytevec_t::raw_begin() noexcept {
 	return &(this->u_.raw_[0]);
 }
-const unsigned char *sbo_t::raw_begin() const {
+const unsigned char *small_bytevec_t::raw_begin() const noexcept {
 	return &(this->u_.raw_[0]);
 }
-unsigned char *sbo_t::raw_end() {
+unsigned char *small_bytevec_t::raw_end() noexcept {
 	return &(this->u_.raw_[0]) + this->u_.raw_.size();
 }
-const unsigned char *sbo_t::raw_end() const {
+const unsigned char *small_bytevec_t::raw_end() const noexcept{
 	return &(this->u_.raw_[0]) + this->u_.raw_.size();
 }
-bool sbo_t::is_big() const {  // private
+bool small_bytevec_t::is_big() const noexcept {  // private
 	return !(this->is_small());
 }
-bool sbo_t::is_small() const {  // private
+bool small_bytevec_t::is_small() const noexcept {  // private
 	return ((this->u_.s_.flags_)&0x80u)==0x80u;
 }
 
--- mtrk_event_t_internal.h
+++ mtrk_event_t_internal.h
@@ -5,9 +5,12 @@
 
 namespace mtrk_event_t_internal {
 
-// 
-// Very simple "'small' std::vector"-like class for managing an array 
-// of unsigned char.  
+//
+// Class small_t
+// Invariants:  
+// -> flags_&0x80u==0x80u
+// -> flags_&0x7Fu==size(); size() <= capacity() <= small_t::size_max
+// -> capacity()==small_t::size_max()
 //
 struct small_t {
 	static constexpr int32_t size_max = 23;
@@ -15,25 +18,24 @@ struct small_t {
 	unsigned char flags_;  // small => flags_&0x80u==0x80u
 	std::array<unsigned char,23> d_;
 	
-	void init();
-	int32_t size() const;
-	constexpr int32_t capacity() const;  // Returns small_t::size_max
+	void init() noexcept;
+	int32_t size() const noexcept;
+	constexpr int32_t capacity() const noexcept;
 	// Can only resize to a value on [0,small_t::size_max]
-	int32_t resize(int32_t);
-	void abort_if_not_active() const;
+	int32_t resize(int32_t) noexcept;
+	void abort_if_not_active() const noexcept;
 
-	unsigned char *begin();
-	const unsigned char *begin() const;
-	unsigned char *end();
-	const unsigned char *end() const;
+	unsigned char *begin() noexcept;
+	const unsigned char *begin() const noexcept;
+	unsigned char *end() noexcept;
+	const unsigned char *end() const noexcept;
 };
 //
 // Class big_t
 // Invariants:  
-// -> If !(flags_&0x80u), p_ is a valid ptr or nullptr; it can always
-//    be tested as if (this->p_) { ... .  
+// -> flags_&0x80u==0x00u
+// -> p_ is a valid ptr or nullptr
 // -> If p_==nullptr, sz_==cap_==0
-//
 //
 struct big_t {
 	static constexpr int32_t size_max = 0x0FFFFFFF;
@@ -47,56 +49,64 @@ struct big_t {
 
 	// init() is meant to be called from an _uninitialized_ state; it does
 	// not and can not test and conditionally delete [] p_.  
-	void init();
+	void init() noexcept;
 	// free_and_reinit() checks p_ and calls delete []; it should only 
 	// be called from an _initialized_ state.  
-	void free_and_reinit();
-	int32_t size() const;
+	void free_and_reinit() noexcept;
+	int32_t size() const noexcept;
 	int32_t resize(int32_t);
 	int32_t reserve(int32_t);
-	int32_t capacity() const;
-	void abort_if_not_active() const;
+	int32_t capacity() const noexcept;
+	void abort_if_not_active() const noexcept;
 	// Deletes p_; object must be initialized before calling
-	void adopt(const pad_t&, unsigned char*, int32_t, int32_t);
+	void adopt(const pad_t&, unsigned char*, int32_t, int32_t) noexcept;
 
-	unsigned char *begin();
-	const unsigned char *begin() const;
-	unsigned char *end();
-	const unsigned char *end() const;
+	unsigned char *begin() noexcept;
+	const unsigned char *begin() const noexcept;
+	unsigned char *end() noexcept;
+	const unsigned char *end() const noexcept;
 };
-class sbo_t {
+// 
+// class small_bytevec_t
+//
+// Very simple "'small' std::vector"-like class for managing an array 
+// of unsigned char.  
+//
+class small_bytevec_t {
 public:
 	static constexpr int32_t size_max = big_t::size_max;
 	static constexpr int32_t capacity_small = small_t::size_max;
 
 	// Constructs a 'small' object w/ size()==0
-	sbo_t();
-	sbo_t(const sbo_t&);  // Copy ctor
-	sbo_t& operator=(const sbo_t&);  // Copy assign
-	sbo_t(sbo_t&&) noexcept;  // Move ctor
-	sbo_t& operator=(sbo_t&&) noexcept;  // Move assign
-	~sbo_t();
+	small_bytevec_t() noexcept;
+	small_bytevec_t(const small_bytevec_t&);  // Copy ctor
+	small_bytevec_t& operator=(const small_bytevec_t&);  // Copy assign
+	small_bytevec_t(small_bytevec_t&&) noexcept;  // Move ctor
+	small_bytevec_t& operator=(small_bytevec_t&&) noexcept;  // Move assign
+	~small_bytevec_t() noexcept;
 
-	int32_t size() const;
-	int32_t capacity() const;
+	int32_t size() const noexcept;
+	int32_t capacity() const noexcept;
 	// int32_t resize(int32_t new_sz);
-	// If 'big' and the new size is <= sbo_t::capacity_small, will cause
-	// a big->small transition.  
+	// If 'big' and the new size is <= small_bytevec_t::capacity_small, 
+	// will cause a big->small transition.  
 	int32_t resize(int32_t);
+	// int32_t reserve(int32_t new_cap);
+	// Will cause big->small, but never small->big transitions.  
 	int32_t reserve(int32_t);
 
 	unsigned char *push_back(unsigned char);
 
-	bool debug_is_big() const;
+	bool debug_is_big() const noexcept;
 
-	unsigned char* begin();
-	const unsigned char* begin() const;
-	unsigned char* end();
-	const unsigned char* end() const;
-	unsigned char* raw_begin();
-	const unsigned char* raw_begin() const;
-	unsigned char* raw_end();
-	const unsigned char* raw_end() const;
+	unsigned char* begin() noexcept;
+	const unsigned char* begin() const noexcept;
+	unsigned char* end() noexcept;
+	const unsigned char* end() const noexcept;
+	unsigned char* raw_begin() noexcept;
+	const unsigned char* raw_begin() const noexcept;
+	unsigned char* raw_end() noexcept; 
+	const unsigned char* raw_end() const noexcept;
 private:
 	union sbou_t {
 		small_t s_;
@@ -105,16 +115,16 @@ private:
 	};
 	sbou_t u_;
 
-	bool is_big() const;
-	bool is_small() const;
-	// Call only on an unitialized object; callas the init() method of the 
+	bool is_big() const noexcept;
+	bool is_small() const noexcept;
+	// Call only on an unitialized object; calls the init() method of the 
 	// corresponding type.  big_t::init() does _not_ free memory.  
-	void init_small();
-	void init_big();
+	void init_small() noexcept;
+	void init_big() noexcept;
 };
 
 static_assert(sizeof(small_t)==sizeof(big_t));
-static_assert(sizeof(small_t)==sizeof(sbo_t));
+static_assert(sizeof(small_t)==sizeof(small_bytevec_t));
 
 };  // namespace mtrk_event_t_internal
 
