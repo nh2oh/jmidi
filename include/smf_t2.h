f2716cec1243ad5bb082b54ceb2efe49976d1bac
ben knowles <ben_junk@ionophore.com>
2020-08-13 15:32:15 -0500

    more smf_t2 work

--- include/smf_t2.h
+++ include/smf_t2.h
@@ -10,20 +10,39 @@ namespace jmid {
 // The mthd data is encoded in fmt_, tdiv_, and mtrks_.size()+uchks.size().  
 //
 // The fmt <-> ntrks relationship mandated by the midi standard is _not_
-// maintained.  
+// maintained.  This is to allow a user to build an object step by step by
+// calling methods, which may require the object to pass through states that
+// are invalid by the midi std.  
 //
+// TODO:
+// - Does the {} construction of the std::vector members allocate?
+//
+
 
 class smf_t2 {
 public:
 	//...
 
-	smf_t2() noexcept;
-	smf_t2(const smf_t2&) noexcept;
-	smf_t2(smf_t2&&) noexcept;
-	smf_t2& operator=(const smf_t2&) noexcept;
-	smf_t2& operator=(smf_t2&&) noexcept;
-	~smf_t2() noexcept;
+	smf_t2();
+	smf_t2(std::int32_t, std::int32_t, jmid::time_division_t);  // ntrks, fmt, tdiv
+	smf_t2(const smf_t2&);
+	smf_t2(smf_t2&&);
+	smf_t2& operator=(const smf_t2&);
+	smf_t2& operator=(smf_t2&&);
+	~smf_t2();
 
+	std::int64_t size() const;
+	std::int64_t nchunks() const;
+	std::int64_t ntrks() const;
+	std::int64_t nuchks() const;
+	std::int64_t nbytes() const;
+
+
+	//
+	// MThd data accessors/modifiers
+	//
+	std::int32_t format() const;
+	jmid::time_division_t time_division() const;
 
 private:
 	std::vector<jmid::mtrk_t> mtrks_ {};
--- src/smf_t2.cpp
+++ src/smf_t2.cpp
@@ -4,10 +4,54 @@
 
 
 
-jmid::smf_t2::smf_t2() noexcept {
-
+jmid::smf_t2::smf_t2() {
 	//...
 }
+
+jmid::smf_t2::smf_t2(std::int32_t ntrks, std::int32_t fmt, jmid::time_division_t tdiv) {
+	if (ntrks < 0) {
+		ntrks = 0;
+	}
+	this->mtrks_.resize(ntrks);
+	if ((fmt != 0) && (fmt != 1) && (fmt != 2)) {
+		fmt = 0;
+	}
+	this->fmt_ = fmt;
+	this->tdiv_ = tdiv;
+}
+
+
+
+
+
+
+std::int64_t jmid::smf_t2::size() const {
+	return this->ntrks();
+}
+std::int64_t jmid::smf_t2::nchunks() const {
+	return this->chunkorder_.size();
+}
+std::int64_t jmid::smf_t2::ntrks() const {
+	return this->mtrks_.size();
+}
+std::int64_t jmid::smf_t2::nuchks() const {
+	return this->uchks_.size();
+}
+std::int64_t jmid::smf_t2::nbytes() const {
+	return 0;  // TODO
+}
+
+
+
+
+
+std::int32_t jmid::smf_t2::format() const {
+	return this->fmt_;
+}
+jmid::time_division_t jmid::smf_t2::time_division() const {
+	return this->tdiv_;
+}
+
 
 
 
